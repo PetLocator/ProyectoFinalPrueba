4cecec690b4a4ea8c55ecc30346ed98e203a42ff
PetLocator <locatorpet5@gmail.com>
2017-05-09 17:25:24 +0200

    Initial commit

---
+++ app/src/main/java/monlau/proyectofinal/MainActivity.java
@@ -0,0 +1,28 @@
+package monlau.proyectofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio);
+        logo = (ImageView) findViewById(R.id.imageView);
+        aparecerLogo();
+        //
+
+    }
+    private void aparecerLogo() {
+        Animation alpha, alpha2;
+        alpha = AnimationUtils.loadAnimation(this, R.anim.logo);
+        logo.startAnimation(alpha);
+        //cambiarPantalla();
+
+    }
+}
